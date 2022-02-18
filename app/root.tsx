ed77908cabbe0c830af558401d43988a3858a194
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2022-02-18 17:04:27 -0300

    add html prefix

--- app/root.tsx
+++ app/root.tsx
@@ -113,7 +113,7 @@ function Document({
   theme?: Theme
 }) {
   return (
-    <html lang="en">
+    <html lang="en" prefix="og: http://ogp.me/ns#">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
