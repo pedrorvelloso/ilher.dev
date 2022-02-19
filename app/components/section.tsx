fdc264d106c7b6bd69f310816d76e33eee9427ae
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2022-02-19 19:02:47 -0300

    add stack section animation

--- app/components/section.tsx
+++ app/components/section.tsx
@@ -16,15 +16,11 @@ export const Section: React.FC<SectionProps> = ({
 }) => {
   return (
     <Tag
-      className={clsx(
-        className,
-        'mx-10vw lg:mx-auto max-w-screen-lg px-0 lg:px-12',
-        {
-          'py-12': featured,
-          'max-w-screen-lg': !extrapolate,
-          'max-w-screen-xl': extrapolate,
-        },
-      )}
+      className={clsx(className, 'mx-10vw lg:mx-auto px-0 lg:px-12', {
+        'py-12': featured,
+        'max-w-screen-lg': !extrapolate,
+        'max-w-screen-xl': extrapolate,
+      })}
     >
       {children}
     </Tag>
--- app/components/sections/stack-section.tsx
+++ app/components/sections/stack-section.tsx
@@ -1,17 +1,41 @@
+import { motion } from 'framer-motion'
+
 import { CodeWindow } from '~/components/code-window'
 import { Section } from '~/components/section'
 import { backendStack, frontendStack, infraStack } from '~/utils/stack'
 
 export const StackSection = () => {
+  const childVariants = {
+    initial: { opacity: 0, y: 25 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  }
+
   return (
-    <div className="bg-accent py-8">
-      <Section
-        className="grid grid-cols-1 gap-y-4 gap-x-4 xl:gap-x-8 lg:grid-cols-3 justify-items-center mx-0 lg:mx-auto"
-        extrapolate
-      >
-        <CodeWindow title="frontend.json" code={frontendStack} />
-        <CodeWindow title="backend.json" code={backendStack} />
-        <CodeWindow title="infra.json" code={infraStack} />
+    <div className="bg-accent">
+      <Section className="py-8 mx-0 lg:mx-auto" extrapolate>
+        <motion.div
+          initial="initial"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={{
+            visible: {
+              opacity: 1,
+              transition: { staggerChildren: 0.2, duration: 0.5 },
+            },
+            initial: { opacity: 0 },
+          }}
+          className="grid grid-cols-1 gap-y-4 gap-x-4 xl:gap-x-8 lg:grid-cols-3 justify-items-center w-full"
+        >
+          <motion.div className="w-full" variants={childVariants}>
+            <CodeWindow title="frontend.json" code={frontendStack} />
+          </motion.div>
+          <motion.div className="w-full" variants={childVariants}>
+            <CodeWindow title="backend.json" code={backendStack} />
+          </motion.div>
+          <motion.div className="w-full" variants={childVariants}>
+            <CodeWindow title="infra.json" code={infraStack} />
+          </motion.div>
+        </motion.div>
       </Section>
     </div>
   )
