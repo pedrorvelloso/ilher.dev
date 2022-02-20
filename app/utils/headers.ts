7ad2ca090f30c18547486ed488f9135858684edb
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2022-02-20 10:54:01 -0300

    testing something with swr header

--- app/utils/headers.ts
+++ app/utils/headers.ts
@@ -17,5 +17,5 @@ export const getHeaders: HeadersFunction = ({ loaderHeaders }) => {
 const SECOND_PER_YEAR = 3.154e7
 
 export const Swr = {
-  'Cache-Control': `max-age=0, s-maxage=1, stale-while-revalidate=${SECOND_PER_YEAR}`,
+  'Cache-Control': `stale-while-revalidate=${SECOND_PER_YEAR}`,
 }
